af47b9be57744bcd178cf770b0f2043fbb5b6617
Chi <cheez2012@gmail.com>
2025-03-14 10:11:49 -0700

    closing local

--- server/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineEventHandler, sendRedirect } from "h3";
-
-// We need to disable this route completely - just return undefined to let Vercel handle it
-export default defineEventHandler((event) => {
-  // Forward to the SPA by sending a redirect
-  return sendRedirect(event, '/index.html', 302);
-});
